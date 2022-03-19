ca35e2d44a53608b1bd25e38f7ad938406359f0b
Rohit Kanrar <rohitk@iastate.edu>
2022-03-19 17:35:22 -0500

    changed renv cache location

--- here/renv_install.sh
+++ here/renv_install.sh
@@ -2,7 +2,7 @@
 
 docker run --rm \
     -e "RENV_PATHS_CACHE=/home/shiny/renv/cache" \
-    -v /home/robert/code/R/renv-cache:/home/shiny/renv/cache \
+    -v /home/.cache/R/renv/cache/v5/R-4.1/x86_64-pc-linux-gnu:/home/shiny/renv/cache \
     -v `pwd`:/home/shiny/project \
     renv-test:latest Rscript -e "renv::restore(confirm = FALSE)" -e "renv::isolate()"
 
